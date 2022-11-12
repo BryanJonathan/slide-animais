9e9ad2cc5d9f5a5ff4bd24da9c893915d91cc882
Bryan Jonathan <bryan.jonathan97@gmail.com>
2022-11-12 15:21:37 -0300

    primeiro commit

---
+++ js/debounce.js
@@ -0,0 +1,11 @@
+/* eslint-disable indent */
+export default function debounce(callback, delay) {
+  let timer;
+  return (...args) => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+      timer = null;
+    }, delay);
+  };
+}
